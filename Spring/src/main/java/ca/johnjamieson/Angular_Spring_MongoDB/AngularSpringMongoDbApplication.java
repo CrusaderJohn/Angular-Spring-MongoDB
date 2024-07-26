8af0d89f0050681694c5b810812b1d83f38c6cc8
John Jamieson <CrusaderJohn@JohnJamieson.ca>
2024-07-25 21:23:01 -0400

    Spring + Angular

---
+++ Spring/src/main/java/ca/johnjamieson/Angular_Spring_MongoDB/AngularSpringMongoDbApplication.java
@@ -0,0 +1,13 @@
+package ca.johnjamieson.Angular_Spring_MongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringMongoDbApplication.class, args);
+	}
+
+}
---
+++ Spring/src/test/java/ca/johnjamieson/Angular_Spring_MongoDB/AngularSpringMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.johnjamieson.Angular_Spring_MongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularSpringMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
